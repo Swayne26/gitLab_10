051f68a57ebd51c21c38f7d1e83398bdfcedb4b7
swayn <swayn@192.168.0.7>
2022-05-17 05:03:25 +0530

    commit 1

---
+++ git_Lab_10/src/git_Lab_10/Orange.java
@@ -0,0 +1,16 @@
+package git_Lab_10;
+
+public class Orange {
+String color;
+Orange(String color)
+{
+this.color=color;
+}
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+	Orange greenApple = new Orange("Green");
+	String color = greenApple.getColor();
+	System.out.println("The color of apple is" + color);
+
+}
+}
